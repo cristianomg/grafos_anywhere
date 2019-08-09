b52f054eba9fd5621bdef4314891c135aa37cce8
cristiano Macedo <cristiano.macedo@souunit.com.br>
2019-08-08 21:50:46 -0300

    criação do menu, conf do grafo por json, inicio do algoritmo de busca dfs.

---
+++ estruturas_de_dados/fila.py
@@ -0,0 +1,20 @@
+from listas import lista_duplamente_ligada
+
+class Fila:
+
+	def __init__(self):
+		self.__elementos = lista_duplamente_ligada.ListaDuplamenteLigada()
+
+	def enfilerar(self,elemento):
+		self.__elementos.inserir(elemento)
+
+	def desenfilerar(self):
+		if self.__elementos.tamanho >= 1:
+			elemento = self.__elementos.recuperar_elemento_no(0)
+			self.__elementos.remover_pos(0)
+			return elemento
+		else:
+			return None 
+
+	def __str__(self):
+		return self.__elementos.__str__()
